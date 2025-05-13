55c2307a630ab1db80490620e80a9c88d8460b11
Abdul Basit <163563761+theabdulbasitt@users.noreply.github.com>
2025-05-14 00:22:07 +0500

    Initial commit

--- config/api.ts
+++ config/api.ts
@@ -1,5 +1,5 @@
 export const DEEPGRAM_API_KEY = '';
-//export const DEEPGRAM_API_KEY= '155b4fb697661b258f2d272607c69bfd8f1624bf';
+//export const DEEPGRAM_API_KEY= '';
 
 export const ASSEMBLY_AI_KEY = ''; // Replace with your Assembly AI key
 export const GEMINI_API_KEY = ''; 
